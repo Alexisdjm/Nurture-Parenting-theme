9753e78c2d0849b35df2c1fd68cb073238a75e62
Alexisdjm <alexis.jm29@gmail.com>
2022-07-26 23:17:53 -0400

    carousel

--- assets/slider.js
+++ assets/slider.js
@@ -44,15 +44,17 @@ const productcontainer = [...document.querySelectorAll('.carousel-element')];
 const arrownext = [...document.querySelectorAll('.arrownext')];
 const arrowprev = [...document.querySelectorAll('.arrowprev')];
 
-productcontainer.forEach((item, i) => {
-  let containerdimensions = item.getBoundingClientRect();
-  let containerwidth = containerdimensions.width;
-
-  arrownext[i].addEventListener('click', () => {
-    item.scrollLeft += containerwidth;
-  })
-
-  arrowprev[i].addEventListener('click', () => {
-    item.scrollLeft -= containerwidth;
-  })
-})
+if (productcontainer) {
+  productcontainer.forEach((item, i) => {
+    let containerdimensions = item.getBoundingClientRect();
+    let containerwidth = containerdimensions.width;
+  
+    arrownext[i].addEventListener('click', () => {
+      item.scrollLeft += containerwidth;
+    })
+  
+    arrowprev[i].addEventListener('click', () => {
+      item.scrollLeft -= containerwidth;
+    })
+  }) 
+}
