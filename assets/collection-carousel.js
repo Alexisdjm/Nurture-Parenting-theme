dd7e5f6ce6bcf46ea7159d255f176af7a1ac41ec
Alexisdjm <alexis.jm29@gmail.com>
2022-07-27 00:12:06 -0400

    wq

--- assets/collection-carousel.js
+++ assets/collection-carousel.js
@@ -1,22 +1,14 @@
 const carousel1 = document.querySelector('.carousel-container');
-const slider1 = document.querySelector('.upper-carousel-container');
 
 const arrownext = document.querySelector('.arrownext');
 const arrowprev = document.querySelector('.arrowprev');
 let direction1;
 
 arrownext.addEventListener('click', function() {
-  direction1 = -1;
-  carousel1.style.justifyContent = 'flex-start';
-  slider1.style.transform = 'translate(-40%)';  
+  console.log("next")
 });
 
 arrowprev.addEventListener('click', function() {
-  if (direction1 === -1) {
-    direction1 = 1;
-    slider1.appendChild(slider.firstElementChild);
-  }
-  carousel1.style.justifyContent = 'flex-end';    
-  slider1.style.transform = 'translate(40%)';  
+ console.log("prev")
   
 });
