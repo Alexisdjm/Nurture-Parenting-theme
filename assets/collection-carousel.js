9124c81fb3907541af11383252cd7d06cf774e85
Alexisdjm <alexis.jm29@gmail.com>
2022-08-07 19:08:46 -0400

    slider products

--- assets/collection-carousel.js
+++ assets/collection-carousel.js
@@ -1,48 +1,35 @@
-// const carousel1 = document.querySelector('.carousel-container');
-// const items = document.querySelectorAll('.container-items')
+let span = document.getElementsByTagName('span');
+	let product = document.getElementsByClassName('product')
+	let product_page = Math.ceil(product.length/4);
+	let l = 0;
+	let movePer = 25.34;
+	let maxMove = 203;
+	// mobile_view	
+	let mob_view = window.matchMedia("(max-width: 768px)");
+	if (mob_view.matches)
+	 {
+	 	movePer = 50.36;
+	 	maxMove = 504;
+	 }
 
-// const arrownext = document.querySelector('.arrownext');
-// const arrowprev = document.querySelector('.arrowprev');
-// let direction1;
+	let right_mover = ()=>{
+		l = l + movePer;
+		if (product == 1){l = 0; }
+		for(const i of product)
+		{
+			if (l > maxMove){l = l - movePer;}
+			i.style.left = '-' + l + '%';
+		}
 
-// console.log(items.length());
-
-// arrownext.addEventListener('click', function() {
-//     console.log("next")
-    
-// });
-
-// arrowprev.addEventListener('click', function() {
-//     console.log("prev")
-    
-// });
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {document.getElementsByClassName("next").disabled = 'true'}
-  if (n < 1) {document.getElementsByClassName("prev").disabled = 'true'}
-  console.log(n);
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+	}
+	let left_mover = ()=>{
+		l = l - movePer;
+		if (l<=0){l = 0;}
+		for(const i of product){
+			if (product_page>1){
+				i.style.left = '-' + l + '%';
+			}
+		}
+	}
+	span[1].onclick = ()=>{right_mover();}
+	span[0].onclick = ()=>{left_mover();}
