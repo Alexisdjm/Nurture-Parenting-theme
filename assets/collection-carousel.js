4147f6988b749963609d8df5fdb27303911c9b73
Alexisdjm <alexis.jm29@gmail.com>
2022-08-07 18:33:04 -0400

    slider products

--- assets/collection-carousel.js
+++ assets/collection-carousel.js
@@ -37,7 +37,7 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.left = "-100px";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
