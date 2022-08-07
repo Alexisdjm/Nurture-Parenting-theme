918c4604dc6dc5f610ef0f5abc5418aefa6e615e
Alexisdjm <alexis.jm29@gmail.com>
2022-08-07 18:27:49 -0400

    slider products

--- assets/collection-carousel.js
+++ assets/collection-carousel.js
@@ -37,7 +37,12 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    if (i == 0) {slides[i].style.left = "0px";}
+    if (i == 1) {slides[i].style.left = "-740px";}
+    if (i == 2) {slides[i].style.left = "-1480px";}
+    if (i == 3) {slides[i].style.left = "-2220px";}
+    if (i == 4) {slides[i].style.left = "-2960px";}
+    
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
