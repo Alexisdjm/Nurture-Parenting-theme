aece349c784c50149f66aeb427f6d63d02d32d5f
Alexisdjm <alexis.jm29@gmail.com>
2022-08-09 21:04:25 -0400

    ebook section js

--- assets/ebook-carousel.js
+++ assets/ebook-carousel.js
@@ -1,29 +1,29 @@
-let span = document.getElementsByClassName('ebook-arrow');
+let arrow = document.getElementsByClassName('ebook-arrow');
 	let ebook = document.getElementsByClassName('ebook')
 	let ebook_page = Math.ceil(ebook.length/4);
 	let l = 0;
-	let movePer = 40;
-	let maxMove = 203;
+	let movement = 40;
+	let limitmove = 203;
 	// mobile_view	
 	let mob_view = window.matchMedia("(max-width: 768px)");
 	if (mob_view.matches)
 	 {
-	 	movePer = 50.36;
-	 	maxMove = 504;
+	 	movement = 50.36;
+	 	limitmove = 504;
 	 }
 
 	let right_mover = ()=>{
-		l = l + movePer;
+		l = l + movement;
 		if (ebook == 1){l = 0; }
 		for(const i of ebook)
 		{
-			if (l > maxMove){l = l - movePer;}
+			if (l > limitmove){l = l - movement;}
 			i.style.left = '-' + l + '%';
 		}
 
 	}
 	let left_mover = ()=>{
-		l = l - movePer;
+		l = l - movement;
 		if (l<=0){l = 0;}
 		for(const i of ebook){
 			if (ebook_page>1){
@@ -31,5 +31,5 @@ let span = document.getElementsByClassName('ebook-arrow');
 			}
 		}
 	}
-	span[1].onclick = ()=>{right_mover();}
-	span[0].onclick = ()=>{left_mover();}
+	arrow[1].onclick = ()=>{right_mover();}
+	arrow[0].onclick = ()=>{left_mover();}
